a8950fdf7e82d84914fdcd040dd8a77221c2b904
Przemek <pzender@hotmail.com>
2025-03-15 13:04:44 +0100

    add vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
-module.exports = {
+// vue.config.js
+const { defineConfig } = require('@vue/cli-service')
+
+module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production' ? '/where-this/' : '/',
-}
+})
